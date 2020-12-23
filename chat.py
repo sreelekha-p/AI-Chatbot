0ecd28929909e7f7db985f06396c36967bf79f70
sreelekha-p <71914526+sreelekha-p@users.noreply.github.com>
2020-12-22 22:53:38 -0600

    Code updated

--- chat.py
+++ chat.py
@@ -1,15 +1,14 @@
+#importing the necessary packages required
 import nltk
 from nltk.stem import WordNetLemmatizer
-
 lemmatizer = WordNetLemmatizer()
 import pickle
 import numpy as np
-
+import json
+import random
 from keras.models import load_model
 
 model = load_model('chatbot_model.h5')
-import json
-import random
 
 intents = json.loads(open('intents.json').read())
 words = pickle.load(open('words.pkl', 'rb'))
